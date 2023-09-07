89215215a262cd93941babbdf551c84eb1f3c01f
hong-brother <robbins.hs@gmail.com>
2023-09-08 00:22:31 +0900

    [feature] #9 - 프리즈마 셋팅

--- src/api/health-check/health.module.ts
+++ src/api/health-check/health.module.ts
@@ -3,10 +3,12 @@ import { HealthController } from './health.controller';
 import { TerminusModule } from '@nestjs/terminus';
 import { HealthService } from './health.service';
 import { HttpModule } from '@nestjs/axios';
+import { PrismaModule } from '../../common/providers/database/postgresql/prisma.module';
+import { TweetsRepository } from '../../common/repositories/tweets.repository';
 
 @Module({
-  imports: [TerminusModule, HttpModule],
-  providers: [HealthService],
+  imports: [TerminusModule, HttpModule, PrismaModule],
+  providers: [HealthService, TweetsRepository],
   controllers: [HealthController],
 })
 export class HealthModule {}
